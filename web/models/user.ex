c3f5ccd402371a0fe283b5af45c35a35d27ae942
Carl Graff <carl.graff@cox.net>
2018-08-17 15:33:24 -0700

    Add User Email to comments

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,10 +1,14 @@
 defmodule Discuss.User do
 	use Discuss.Web, :model
 
+  @derive {Poison.Encoder, only: [:email]}
+
   schema "users" do
     field :email, :string
     field :provider, :string
     field :token, :string
+    has_many :topics, Discuss.Topic
+    has_many :commments, Discuss.Comment
 
     timestamps()
   end
